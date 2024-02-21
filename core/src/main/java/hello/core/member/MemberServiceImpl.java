0d74137d3a2c6a27ab2c572979a1013ac60224b1
DongHyeok Lim <jh07050@gmail.com>
2024-02-21 16:44:03 +0900

    feat: Spring 핵심 원리: 스프링 빈 조회

---
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,27 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService {
+
+    // 이전에는 private final MemberRepository memberRepository = new MemberRepository();
+    // 위와 같이 객체를 주입하여사용하였으나 이는 DIP에 위배됨
+
+    // 오직 MemberRepository 인터페이스에만 의존
+    private final MemberRepository memberRepository;
+
+    // 이 생성자에서는 어떠한 구현 객체가 들어올 지 알 수 없음
+    // 어떠한 구현 객체가 주입될지는 AppConfig에서만 결정 가능
+    // 의존관계는 외부에 맡기고 실행에만 집중
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
