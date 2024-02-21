b39b2581ba01ea5f74489db8244efcfe74b25dcb
DongHyeok Lim <jh07050@gmail.com>
2024-02-21 20:06:16 +0900

    feat: @Configuration과 바이트코드 조작

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -24,4 +24,10 @@ public class MemberServiceImpl implements MemberService {
     public Member findMember(Long memberId) {
         return memberRepository.findById(memberId);
     }
+
+
+    // Configuration 테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
 }
