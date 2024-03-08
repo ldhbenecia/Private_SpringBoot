714cd77fe7afc3870fe978d835968ab46a2796a5
DongHyeok Lim <jh07050@gmail.com>
2024-03-08 20:51:53 +0900

    feat: 양방향 연관관계 설정 및 연관관계 편의 메서드

--- hello-jpa/src/main/java/hellojpa/domain/Team.java
+++ hello-jpa/src/main/java/hellojpa/domain/Team.java
@@ -1,9 +1,9 @@
 package hellojpa.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Team {
@@ -11,6 +11,17 @@ public class Team {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
+    }
 
     public Long getId() {
         return id;
