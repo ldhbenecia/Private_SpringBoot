60784c78358c9ee43aec6d4032cbbd6688c0055b
DongHyeok Lim <jh07050@gmail.com>
2024-05-18 23:14:58 +0900

    test: @EntityGraph 테스트

--- spring-data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ spring-data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -183,4 +183,34 @@ class MemberRepositoryTest {
         Assertions.assertThat(resultCount).isEqualTo(3);
 
     }
+
+    @Test
+    public void findMemberLazy() throws Exception {
+        // given
+        // member1 -> teamA
+        // member2 -> teamB
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        teamRepository.save(teamA);
+        teamRepository.save(teamB);
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 10, teamB);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        em.flush();
+        em.clear();
+
+        // when N + 1 (쿼리를 한번 날렸는데 team을 찾을 때마다 쿼리가 날아가서 쿼리가 추가된다.)
+        // select Member
+        List<Member> members = memberRepository.findAll();
+        for (Member member : members) {
+            System.out.println("member = " + member.getUsername());
+            System.out.println("member.team = " + member.getTeam().getName());
+        }
+
+        // then
+
+    }
 }
