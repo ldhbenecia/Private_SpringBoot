9d9f41aaef4ab606bc966be7bd0792cfa4aff46f
DongHyeok Lim <jh07050@gmail.com>
2024-05-18 23:14:51 +0900

    feat: @EntityGraph

--- spring-data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ spring-data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package study.datajpa.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -38,4 +39,18 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     @Modifying(clearAutomatically = true) // 이게 있어야 executeUpdate가 나감, update로 변경할 때는 이 어노테이션을 붙여줘야함
     @Query("update Member m set m.age = m.age + 1 where m.age >= :age")
     int bulkAgePlus(@Param("age") int age);
+
+    @Query("select m from Member m left join fetch m.team")
+    List<Member> findMemberFetchJoin();
+
+    @Override
+    @EntityGraph(attributePaths = {"team"})
+    List<Member> findAll();
+
+    @EntityGraph(attributePaths = {"team"})
+    @Query("select m from Member m")
+    List<Member> findMemberEntityGraph();
+
+    @EntityGraph(attributePaths = {"team"})
+    List<Member> findByUsername(@Param("username") String username);
 }
